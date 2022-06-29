3f9c32c85fca042e39d4292a651b7696a580fc8d
Jhone Dourado <72326178+jhonedourado@users.noreply.github.com>
2022-06-29 09:50:20 -0300

    Primeiro envio

---
+++ produtos.js
@@ -0,0 +1,56 @@
+const items = [
+    {
+        id: 0,
+        quantidade: 0,
+        nome: 'Do Mil ao Milhão',
+        img: './domilaomilhao.jpg',
+        preco: 'R$ 19,99',
+        dividido: '5x de R$ 3,99 sem juros',
+        descricao: ''
+    },
+    {
+        id: 1,
+        quantidade: 0,
+        nome: 'Arrume a sua cama',
+        img: './arrumesuacama.jpg',
+        preco: 'R$ 20,99',
+        dividido: '5x de R$ 4,19 sem juros',
+        descricao: ''
+    },
+    {
+        id: 2,
+        quantidade: 0,
+        nome: 'Ego Transformado',
+        img: './egotransformado.jpg',
+        preco: 'R$ 12,99',
+        dividido: '5x de R$ 2,59 sem juros',
+        descricao: ''
+    },
+    {
+        id: 3,
+        quantidade: 0,
+        nome: 'Bom Dia Espirito Santo',
+        img: './bomdiaespiritosanto.jpg',
+        preco: 'R$ 19,99',
+        dividido: '5x de R$ 3,99 sem juros',
+        descricao: ''
+    },
+    {
+        id: 4,
+        quantidade: 0,
+        nome: 'As Crônicas de Narnia',
+        img: './narnia.jpg',
+        preco: 'R$ 44,99',
+        dividido: '5x de R$ 8,99 sem juros',
+        descricao: ''
+    },
+    {
+        id: 5,
+        quantidade: 0,
+        nome: 'O Poder do Hábito',
+        img: './opoderdohabito.jpg',
+        preco: 'R$ 46,99',
+        dividido: '5x de R$ 9,39 sem juros',
+        descricao: ''
+    }
+]
